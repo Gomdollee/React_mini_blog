f1f3e0ddaf35e7a900142820a8c5f9433e47d2e3
junwon_lee <wjdrbs307@naver.com>
2023-12-08 11:08:53 +0900

    45

--- src/component/ui/Button.jsx
+++ src/component/ui/Button.jsx
@@ -5,15 +5,14 @@ const StyledButton = styled.button`
     padding: 8px 16px;
     font-size: 16px;
     border-width: 1px;
-    border-radius:8px;
+    border-radius: 8px;
     cursor: pointer;
 `;
 
-function Button(props) { 
+function Button(props) {
     const { title, onClick } = props;
 
     return <StyledButton onClick={onClick}>{title || "button"}</StyledButton>;
-
 }
 
 export default Button;
